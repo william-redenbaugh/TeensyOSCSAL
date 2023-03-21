97c39f9faa2d06bfe9ea4d55ae927b0e0f7be81a
William Redenbaugh <wmredenbaugh@gmail.com>
2023-03-21 13:33:59 -0700

    created shared definitions folder, started declaration of the mutex

---
+++ os_shared.h
@@ -0,0 +1,14 @@
+#ifndef _OS_SHARED_H
+#define  _OS_SHARED_H
+
+/**
+ * @brief When the HAL layer is using our TeensyOS layer
+*/
+#define USE_TEENSYOS
+
+/**
+ * @brief When the abstraction layer is targeting Nuttx
+*/
+// #define USE_NUTTX_POSX
+
+#endif
--- os_thread.h
+++ os_thread.h
@@ -1,4 +1,13 @@
 #ifndef _OS_THREAD_H
 #define _OS_THREAD_H
 
+#include "os_shared.h"
+
+#ifdef USE_TEENSYOS
+
+#endif
+
+#ifdef USE_NUTTX_POSX
+
+#endif
 #endif
